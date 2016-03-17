cab3a9cbf5d04cb8d1b0ba483feae4613de5284c
Luke Huang <luke.huang@cloudysys.com>
2016-03-17 16:11:19 +0800

    randomize test number range

--- test/xor_tests.erl
+++ test/xor_tests.erl
@@ -3,8 +3,13 @@
 -include("../include/assignment.hrl").
 
 swap_test() ->
-    {?binding(a, N), ?binding(b, M)} =
-        xor_:swap(?binding(a, 3), ?binding(b, 5)),
-    ?assert(N =:= 5),
-    ?assert(M =:= 3),
+    {{Y,Mt,D},{H,Mn,S}} = calendar:local_time(),
+    random:seed(Y+H, Mt+Mn, D+S),
+    N = random:uniform(1000000),
+    M = random:uniform(1000000) + 1000000,
+    io:fwrite("[test] swapping ~B and ~B...~n", [N, M]),
+    {?binding(a, N1), ?binding(b, M1)} =
+        xor_:swap(?binding(a, N), ?binding(b, M)),
+    ?assert(N =:= M1),
+    ?assert(M =:= N1),
     ok.
